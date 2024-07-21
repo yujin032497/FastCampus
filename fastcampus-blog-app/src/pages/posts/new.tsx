57f5df510378086c1138f245cfb13f98b00b26aa
YuJinKim <yu_jin456@naver.com>
2024-07-22 00:17:42 +0900

    # form 컴포넌트 작업

--- fastcampus-blog-app/src/pages/posts/new.tsx
+++ fastcampus-blog-app/src/pages/posts/new.tsx
@@ -1,12 +1,12 @@
 import Header from "components/Header";
 import Footer from "components/Footer";
+import PostForm from "components/PostForm";
 
 export default function PostNew() {
     return (
         <>
             <Header />
-            <div>Post Form</div>
-            <Footer />
+            <PostForm />
         </>
     );
 }
