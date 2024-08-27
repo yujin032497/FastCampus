124ca04f9b466080d401f3ea3ba9070dd00f447c
YuJinKim <yu_jin456@naver.com>
2024-08-27 22:23:12 +0900

    # 로그인 페이지 세팅하기

--- fastcampus-blog-app/src/pages/login/index.tsx
+++ fastcampus-blog-app/src/pages/login/index.tsx
@@ -1,3 +1,12 @@
+import Header from "components/Header";
+import LoginForm from "components/LoginForm";
+import Footer from "components/Footer";
 export default function LoginPage() {
-    return <h2>Login</h2>;
+    return (
+        <>
+            <Header />
+            <LoginForm />
+            <Footer />
+        </>
+    );
 }
