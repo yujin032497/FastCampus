49b7140f8396596215f2ffeb30df04b945eee84b
YuJinKim <yu_jin456@naver.com>
2024-08-27 21:46:19 +0900

    # Firebase 세팅하기

--- fastcampus-blog-app/src/index.tsx
+++ fastcampus-blog-app/src/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import firebase from "firebaseApp";
+
+console.log(firebase);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
