3fdfbf5a58e7bd47301828bb3471b8a90eed66f8
YuJinKim <yu_jin456@naver.com>
2024-09-05 21:57:40 +0900

    # Firebase Auth로 로그아웃 실습

--- fastcampus-blog-app/src/components/Profile.tsx
+++ fastcampus-blog-app/src/components/Profile.tsx
@@ -1,18 +1,40 @@
-import { Link } from "react-router-dom";
+
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+
+import { toast } from "react-toastify";
+// import { Link } from "react-router-dom";
+
+const onSignOut = async () => {
+    try {
+        const auth = getAuth(app);
+        await signOut(auth);
+        toast.success("로그아웃 되었습니다.");
+    } catch (error: any) {
+        console.log(error);
+        toast.error(error?.code)
+    }
+   
+};
 
 export default function Profile() {
+    const auth = getAuth(app);
+    //console.log(auth);
     return (
         <div className="profile__box">
             <div className="flex__box-lg">
                 <div className="profile__image" />
                 <div>
-                    <div className="profile__email">test@test.com</div>
-                    <div className="profile__name">김유저</div>
+                    <div className="profile__email">{auth?.currentUser?.email}</div>
+                    <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div>
                 </div>
             </div>
-            <Link to="/" className="profile__logout">
+            <div role="presentation" className="profile__logout" onClick={onSignOut}>
                 로그아웃
-            </Link>
+            {/* <Link to="/" className="profile__logout">
+                로그아웃
+            </Link>  */}
+            </div>
         </div>
     );
 }
