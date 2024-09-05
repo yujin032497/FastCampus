be7d6e1341e2223992b508f1294fd2aa03529b5c
YuJinKim <yu_jin456@naver.com>
2024-09-05 22:42:36 +0900

    # context api로 사용자 정보 관리하기

--- fastcampus-blog-app/src/components/Profile.tsx
+++ fastcampus-blog-app/src/components/Profile.tsx
@@ -1,6 +1,7 @@
-
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 
 import { toast } from "react-toastify";
 // import { Link } from "react-router-dom";
@@ -18,15 +19,20 @@ const onSignOut = async () => {
 };
 
 export default function Profile() {
-    const auth = getAuth(app);
+    // const auth = getAuth(app);
     //console.log(auth);
+    const { user } = useContext(AuthContext);
+    // console.log(user);
+
     return (
         <div className="profile__box">
             <div className="flex__box-lg">
                 <div className="profile__image" />
                 <div>
-                    <div className="profile__email">{auth?.currentUser?.email}</div>
-                    <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div>
+                    {/* <div className="profile__email">{auth?.currentUser?.email}</div>
+                    <div className="profile__name">{auth?.currentUser?.displayName || '사용자'}</div> */}
+                    <div className="profile__email">{user?.email}</div>
+                    <div className="profile__name">{user?.displayName || '사용자'}</div>
                 </div>
             </div>
             <div role="presentation" className="profile__logout" onClick={onSignOut}>
