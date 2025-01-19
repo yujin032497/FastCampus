7bb39b862749b470af262837171d95f323bc66fd
YuJinKim <yu_jin456@naver.com>
2025-01-19 19:45:26 +0900

    # 댓글 UI 작업

--- fastcampus-blog-app/src/components/PostDetail.tsx
+++ fastcampus-blog-app/src/components/PostDetail.tsx
@@ -1,74 +1,78 @@
 import { useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom"
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { PostProps } from "./PostList";
 import { db } from "firebaseApp";
 import { doc, getDoc, deleteDoc } from "firebase/firestore";
 import { toast } from "react-toastify";
 import Loader from "./Loader";
+import Comments from "./Comments";
 
 export default function PostDetail() {
-    const [post, setPost] = useState<PostProps | null>(null);
-    const params = useParams();
-    // console.log(params?.id);
-    const navigate = useNavigate();
+  const [post, setPost] = useState<PostProps | null>(null);
+  const params = useParams();
+  // console.log(params?.id);
+  const navigate = useNavigate();
 
-    const getPost = async (id: string) => {
-        if (id) {
-            const docRef = doc(db, "posts", id);
-            const docSnap = await getDoc(docRef);
-            // console.log(docSnap?.data());
-            setPost({ id: docSnap.id, ...docSnap.data() as PostProps });
-        }
-    };
+  const getPost = async (id: string) => {
+    if (id) {
+      const docRef = doc(db, "posts", id);
+      const docSnap = await getDoc(docRef);
+      // console.log(docSnap?.data());
+      setPost({ id: docSnap.id, ...docSnap.data() as PostProps });
+    }
+  };
 
-    const handleDelete = async () => {
-        const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
-        if (confirm && post && post.id) {
-            await deleteDoc(doc(db, "posts", post.id));
-            toast.success("게시글을 삭제했습니다.");
-            navigate("/");
-        }
-    };
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast.success("게시글을 삭제했습니다.");
+      navigate("/");
+    }
+  };
 
-    // console.log(post);
+  // console.log(post);
 
-    useEffect(() => {
-        if (params?.id) getPost(params?.id);
-    }, [params?.id]);
-    return (
-        <>
-            <div className="post__detail">
-                {post ? (
-                    <div className="post__box">
-                        <div className="post__title">{post?.title}</div>
-                        <div className="post__profile-box">
-                            <div className="post__profile" />
-                            <div className="post__author-name">{post?.email}</div>
-                            <div className="post__date">{post?.createdAt}</div>
-                        </div>
-                        <div className="post__utils-box">
-                            {post?.category && (
-                                <div className="post__category">{post?.category}</div>
-                            )}
-                            <div
-                                className="post__delete"
-                                role="presentation"
-                                onClick={handleDelete}
-                            >
-                                삭제
-                            </div>
-                            <div className="post__edit">
-                                <Link to={`/posts/edit/${post?.id}`}>수정</Link>
-                            </div>
-                        </div>
-                        <div className="post__text post__text--pre-wrap">
-                            {post?.content}
-                        </div>
-                    </div>
-                ) : (
-                    <Loader />
+  useEffect(() => {
+    if (params?.id) getPost(params?.id);
+  }, [params?.id]);
+  return (
+    <>
+      <div className="post__detail">
+        {post ? (
+          <>
+            <div className="post__box">
+              <div className="post__title">{post?.title}</div>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">{post?.email}</div>
+                <div className="post__date">{post?.createdAt}</div>
+              </div>
+              <div className="post__utils-box">
+                {post?.category && (
+                  <div className="post__category">{post?.category}</div>
                 )}
+                <div
+                  className="post__delete"
+                  role="presentation"
+                  onClick={handleDelete}
+                >
+                  삭제
+                </div>
+                <div className="post__edit">
+                  <Link to={`/posts/edit/${post?.id}`}>수정</Link>
+                </div>
+              </div>
+              <div className="post__text post__text--pre-wrap">
+                {post?.content}
+              </div>
             </div>
-        </>
-    );
+            <Comments />
+          </>
+        ) : (
+          <Loader />
+        )}
+      </div>
+    </>
+  );
 }
